df10eb9adf853e9fbc465abb14270a3d62d04690
Vicky8061 <vickykhalasi0318@gmail.com>
2026-08-13 21:32:00 +0530

    Audit and improve API controllers

--- app/Http/Controllers/Api/AdminBookingController.php
+++ app/Http/Controllers/Api/AdminBookingController.php
@@ -9,39 +9,45 @@ use Illuminate\Http\Request;
 
 class AdminBookingController extends Controller
 {
-    public function index(Request $request){
+    /**
+     * Display all bookings.
+     */
+    public function index(Request $request)
+    {
         $query = Booking::with([
             'user',
             'court',
-            'timeSlot'
+            'timeSlot',
         ]);
 
-        //Search by User name or court name
-        if($request->filled('search')){
+        // Search by user name or court name
+        if ($request->filled('search')) {
             $search = $request->search;
 
-            $query->where(function ($q) use($search){
-                $q->whereHas('user',function ($user) use($search){
-                    $user->where('name','like',"%{$search}%");
-                });
-                $q->OrWhereHas('court',function ($court) use($search){
-                    $court->where('name','like',"%{$search}%");
-                });
+            $query->where(function ($q) use ($search) {
 
+                $q->whereHas('user', function ($user) use ($search) {
+                    $user->where('name', 'like', "%{$search}%");
+                })
+
+                    ->orWhereHas('court', function ($court) use ($search) {
+                        $court->where('name', 'like', "%{$search}%");
+                    });
             });
         }
 
-        //filter by booking status
-        if($request->filled('status')){
-            $query->where('booking_status',$request->status);
+        // Filter by booking status
+        if ($request->filled('status')) {
+            $query->where('booking_status', $request->status);
         }
 
-        //Latest / oldest
+        // Sorting
+        switch ($request->sort) {
 
-        switch($request->sort){
             case 'oldest':
                 $query->oldest();
                 break;
+
             case 'latest':
             default:
                 $query->latest();
@@ -49,30 +55,35 @@ class AdminBookingController extends Controller
         }
 
         $bookings = $query->paginate(10);
+
         return response()->json([
-            'success'=>true,
-            'message'=>'Bookings fetched successfuly',
-            'data'=>BookingResource::collection($bookings),
-            'pagination'=>[
-                'current_page'=> $bookings->currentPage(),
-                'last_page'=> $bookings->lastPage(),
-                'per_page'=> $bookings->perPage(),
-                'total'=> $bookings->total(),
-            ]
-        ],200);
+            'success' => true,
+            'message' => 'Bookings fetched successfully.',
+            'data' => BookingResource::collection($bookings),
+            'pagination' => [
+                'current_page' => $bookings->currentPage(),
+                'last_page' => $bookings->lastPage(),
+                'per_page' => $bookings->perPage(),
+                'total' => $bookings->total(),
+            ],
+        ], 200);
     }
 
-    public function show(Booking $booking){
+    /**
+     * Display a specific booking.
+     */
+    public function show(Booking $booking)
+    {
         $booking->load([
             'user',
             'court',
-            'timeSlot'
+            'timeSlot',
         ]);
 
         return response()->json([
-            'success'=>true,
-            'message'=>'Booking fetched successfuly',
-            'data'=> new BookingResource($booking),
-        ],200);
+            'success' => true,
+            'message' => 'Booking fetched successfully.',
+            'data' => new BookingResource($booking),
+        ], 200);
     }
 }
